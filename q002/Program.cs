542c095e58603e87f0ba9613d95b0d4e48bf5344
Faraz Ahmed Khan <31242842+fk03983@users.noreply.github.com>
2018-07-24 03:44:45 +0500

    Add files via upload

---
+++ q002/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /*Author Moosa*/
+        static void Main(string[] args)
+        {
+            int noofinputs = int.Parse(Console.ReadLine());
+            
+            for ( int  i = 0; i < noofinputs; i ++)
+            {
+                long input = long.Parse(Console.ReadLine());
+                long firstterm = 0, secondterm = 2, thirdterm = 0;
+                long sum = firstterm + secondterm;
+                if (input < 2)
+                {
+                    sum = 0;                   
+                }
+                else
+                {
+                    for (long j = 0; thirdterm <= input; j++)
+                    {
+                        thirdterm = firstterm + (4 * secondterm);
+                        firstterm = secondterm;
+                        secondterm = thirdterm;
+                        sum = sum + thirdterm;
+                    }
+                    if (sum >= input)
+                    {
+                        sum = sum - thirdterm;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+            Console.ReadKey();
+        }
+    }
+}
